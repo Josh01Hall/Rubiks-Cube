064246f953af65d5f8d8a5732d7fc3c4c530fec4
Joshua Hall <joshua.hall@sykesseafood.com>
2024-10-17 15:22:05 +0100

    Tweaks

--- virtual_env.ps1
+++ virtual_env.ps1
@@ -1,3 +1,6 @@
+#https://medium.com/@astontechnologies/how-to-setup-a-virtual-development-environment-for-python-with-windows-powershell-4cd34b2f9f9b
+
+
 python -m venv .venv
 
 pip install -r requirements.txt
